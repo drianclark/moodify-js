ed99ea9743787a23937620d9897ae03379d7cea5
drianclark <dreiclark@gmail.com>
2020-02-04 11:48:35 +0000

    midway through authentication

--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
-const express = require('express')
+const express = require('express');
 const fetch = require('node-fetch');
-const app = express()
-const port = 3000
+const app = express();
+const port = 5000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app. get('/api/request_token', async(req, res) => {
+    const params = new URLSearchParams({ 
+        client_id: '44ef850b66114e6ea1d2fd3c9124af70',
+        response_type: 'code',
+        redirect_uri: '/api/request_token/callback',
+        scope: 'user-read-recently-played'
+    });
+
+    let code = await fetch('https://accounts.spotify.com/authorize' + params)
+    let response = code.url;
+});
+
 
 app.get('/test', async (req, res) => {
     let todos = await fetch('http://jsonplaceholder.typicode.com/todos/1')
