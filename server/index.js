8feacb4e117d256065bcedad027fd69858a7dfc9
Jack Bond-Preston <jackbondpreston@outlook.com>
2020-02-04 11:47:38 +0000

    Load authentication data from authentication.json

--- server/index.js
+++ server/index.js
@@ -1,16 +1,22 @@
-const express = require('express')
+const express = require('express');
 const fetch = require('node-fetch');
-const app = express()
-const port = 3000
+const fs = require('fs');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const authData = JSON.parse(fs.readFileSync('authentication.json'));
+
+console.log(`Client ID: ${ authData['client_id'] }, secret: ${ authData['client_secret'] }.`);
+
+
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/test', async (req, res) => {
-    let todos = await fetch('http://jsonplaceholder.typicode.com/todos/1')
-    let json = await todos.json()
+    let todos = await fetch('http://jsonplaceholder.typicode.com/todos/1');
+    let json = await todos.json();
     console.log(json);
-    res.send(json)
+    res.send(json);
     
 })
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
