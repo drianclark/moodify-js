4e1e0f6cfb7d8851c48e90f837316dc884a3fe39
drianclark <dreiclark@gmail.com>
2020-02-24 21:06:49 +0000

    cleaning up old db files

--- db_update_service/index.js
+++ db_update_service/index.js
@@ -8,15 +8,29 @@ requestUpdate();
 setInterval(requestUpdate, msToWaitBeforeUpdate);
 
 async function requestUpdate() {
+  while (true) {
     try {
-        console.log("sent get")
-        var r = await timeout(3000, fetch(url));
-        var json = await r.json();
-        console.log(json);    
-    } catch (error) {
-        console.log(error);
-        console.log("error in GET request, try again in a bit");
+      console.log("sent get")
+      var r = await timeout(3000, fetch(url));
+      var json = await r.json();
+      break;
+      console.log(json);    
+    } 
+    catch (error) {
+      console.log(error);
+      console.log("error in GET request, try again in a bit");
+      if (error == "timeout") {
+        console.log("timeout bitch");
+        // await new Promise(r => setTimeout(r, 3000));
+        // continue
+      }
     }
+
+    finally {
+      await new Promise(r => setTimeout(r, 1000));
+    }
+  }
+    
 }
 
 function timeout(ms, promise) {
